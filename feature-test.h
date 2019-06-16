4c83a1032c2ed2f660f2730a799284ef0902c8b8
Eliah Kagan <degeneracypressure@gmail.com>
2019-06-16 19:49:40 -0400

    extract feature test macro inclusion into its own header file
    This is worthwhile because of the warning suppression for clang.

---
+++ feature-test.h
@@ -0,0 +1,16 @@
+// feature-test.h - feature test macros to define above system header includes
+//  Used by fiemap.c.
+
+#ifndef HAVE_IOCTL_1_FIEMAP_FEATURE_TEST_H_
+#define HAVE_IOCTL_1_FIEMAP_FEATURE_TEST_H_
+
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wreserved-id-macro"
+#endif
+#define _POSIX_C_SOURCE 1
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+
+#endif // ! HAVE_IOCTL_1_FIEMAP_FEATURE_TEST_H_
