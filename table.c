001c75f8109868c7dce04e5228494304c914c97d
Eliah Kagan <degeneracypressure@gmail.com>
2019-06-28 18:15:37 -0400

    started implementing the new interface

--- table.c
+++ table.c
@@ -4,6 +4,7 @@
 
 #include "attribute.h"
 #include "constants.h"
+#include "util.h"
 
 #include <assert.h>
 #include <stdio.h>
@@ -11,12 +12,53 @@
 #include <linux/types.h>
 #include <sys/ioctl.h>
 
+struct tablespec *alloc_tablespec(const int col_count)
+{
+    assert(col_count >= 0);
+
+    return xcalloc(1u, sizeof(struct tablespec)
+                        + sizeof(struct colspec) * (size_t)col_count);
+}
+
+ATTRIBUTE((nonnull))
+static inline __u64 get_raw(const struct fiemap_extent *const fep,
+                            const enum field field)
+{
+    assert(fep);
+
+    switch (field) {
+        case field_logical:     return fep->fe_logical;
+        case field_physical:    return fep->fe_physical;
+        case field_length:      return fep->fe_length;
+    }
+
+    die("unrecognized field type (bug!)");
+}
+
+ATTRIBUTE((nonnull))
+static inline __u64 get(const struct fiemap *const fmp,
+                        const struct colspec *const csp, const __u32 row_index)
+{
+    assert(fmp && csp && row_index < fmp->fm_mapped_extents);
+    const __u64 raw = get_raw(&fmp->fm_extents[row_index], csp->field);
+    return (raw + csp->offset) / csp->divisor;
+}
+
 void populate_widths(struct tablespec *const tsp)
 {
 
 }
 
+ATTRIBUTE((nonnull))
+static void show_labels(const struct tablespec *const tsp)
+{
+    assert(tsp);
+
+}
+
 void show_extent_table(const struct tablespec *const tsp)
 {
+    assert(tsp);
+
 
 }
--- table.h
+++ table.h
@@ -18,21 +18,24 @@ struct colspec {
     const char *label;
     int width;
     enum field field;
+    __u64 offset;
     __u64 divisor;
 };
 
 struct tablespec {
     const struct fiemap *fmp;
-    __u64 offset;
-    int gap;
-    size_t ncols;
+    int gap_width;
+    int col_count;
     struct colspec cols[];
 };
 
-ATTRIBUTE((nonnull))
-void populate_widths(struct tablespec *const tsp);
+ATTRIBUTE((malloc, returns_nonnull))
+struct tablespec *alloc_tablespec(int col_count);
 
 ATTRIBUTE((nonnull))
-void show_extent_table(const struct tablespec *const tsp);
+void populate_widths(struct tablespec *tsp);
+
+ATTRIBUTE((nonnull))
+void show_extent_table(const struct tablespec *tsp);
 
 #endif // ! HAVE_IOCTL_1_FIEMAP_TABLE_H_
