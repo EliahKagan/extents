725e1ab9503a7f4dc2714e141c6a64be583219c9
Eliah Kagan <degeneracypressure@gmail.com>
2019-07-02 21:01:29 -0400

    put a blank line above the table (so not just below it)

--- fiemap.c
+++ fiemap.c
@@ -55,8 +55,9 @@ static __u64 get_offset(const dev_t dev)
         die("can't interpret %s as offset", path);
 
     const __u64 offset = offset_in_sectors * k_sector_size;
-    printf("On block device %u:%u, which starts at byte %llu (sector %llu):\n",
-           maj, min, offset, offset_in_sectors);
+    printf("On block device %u:%u, "
+            "which starts at byte %llu (sector %llu):\n\n",
+            maj, min, offset, offset_in_sectors);
 
     return offset;
 }
