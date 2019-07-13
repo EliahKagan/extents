c98a98457b45ea55b9223da30dfc0fa81394291b
Eliah Kagan <degeneracypressure@gmail.com>
2019-07-13 13:43:29 -0400

    added a TODO about unexpected POSIXLY_CORRECT-like behavior without _GNU_SOURCE

--- feature-test.h
+++ feature-test.h
@@ -7,6 +7,13 @@
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wreserved-id-macro"
 #endif
+// TODO: Find out why GNU getopt() doesn't process options that appear after
+//       non-option arguments with _POSIX_C_SOURCE 2 or _XOPEN_SOURCE, even
+//       when the POSIXLY_CORRECT environment variable is undefined at runtime
+//       and optstring has no leading '+'. _GNU_SOURCE will be required for
+//       getopt_long() anyway, but it would be useful to know why it exhibits
+//       this seemingly undocumented behavior. I do want to support non-leading
+//       options wherever feasible (so definitely when glibc is used).
 #define _GNU_SOURCE
 #ifdef __clang__
 #pragma clang diagnostic pop
