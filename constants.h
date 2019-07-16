a7cc697be999048e7a4ca7e12351f039609ca1ee
Eliah Kagan <degeneracypressure@gmail.com>
2019-07-15 22:08:23 -0400

    added missing final paragraph of the per-file CC0 notice

--- constants.h
+++ constants.h
@@ -7,6 +7,10 @@
 // To the extent possible under law, the author(s) have dedicated all copyright
 // and related and neighboring rights to this software to the public domain
 // worldwide. This software is distributed without any warranty.
+//
+// You should have received a copy of the CC0 Public Domain Dedication along
+// with this software. If not, see
+// <http://creativecommons.org/publicdomain/zero/1.0/>.
 
 #ifndef HAVE_IOCTL_1_FIEMAP_CONSTANTS_H_
 #define HAVE_IOCTL_1_FIEMAP_CONSTANTS_H_
